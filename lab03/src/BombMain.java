d80844fdfe73c61ee03ace6c6483561b16e479b5
your name <rrr870213@berkeley.edu>
2024-06-20 23:39:55 -0700

    Finished Lab 3

--- lab03/src/BombMain.java
+++ lab03/src/BombMain.java
@@ -13,10 +13,17 @@ public class BombMain {
             b.phase0("39291226"); // Figure this out. I wonder where the phases are defined...
         }
         if (phase >= 1) {
-            b.phase1(new int[]{0,9,3,0,8}); // Figure this out next
+            b.phase1(new int[]{0, 9, 3, 0, 8}); // Figure this out next
         }
         if (phase >= 2) {
-            b.phase2("-81201430");
+            // Create a string with 1337 "A " elements and append the correct value
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 1337; i++) { // 1337個元素，索引0-1336
+                sb.append("A ");
+            }
+            sb.append("-81201430"); // 這是我花了一個下午找到的密碼
+            String password = sb.toString();
+            b.phase2(password);
         }
     }
 }
