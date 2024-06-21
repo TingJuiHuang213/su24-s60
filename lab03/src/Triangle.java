26b8243458139ac5a67e0801ec59f48721adaab1
your name <rrr870213@berkeley.edu>
2024-06-20 19:30:38 -0700

    Finished Lab 3

--- lab03/src/Triangle.java
+++ lab03/src/Triangle.java
@@ -10,4 +10,8 @@ public abstract class Triangle {
      public abstract boolean isIsosceles();
      public abstract boolean isScalene();
      public abstract boolean sidesFormTriangle(int side1, int side2, int side3);
+     public abstract boolean pointsFormTriangle(int x1, int y1, int x2, int y2, int x3, int y3);
+     public abstract String triangleType(int side1, int side2, int side3);
+     public abstract int squaredHypotenuse(int side1, int side2);
 }
+
--- lab03/tests/TriangleTest.java
+++ lab03/tests/TriangleTest.java
@@ -1,12 +1,75 @@
 import org.junit.jupiter.api.Test;
 import static com.google.common.truth.Truth.assertThat;
-import org.junit.jupiter.api.function.Executable;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TriangleTest {
-
     public Triangle getNewTriangle() {
-        return new ConcreteTriangle();
+        return new Triangle() {
+            private int side1, side2, side3;
+
+            @Override
+            public void setSides(int side1, int side2, int side3) {
+                if (sidesFormTriangle(side1, side2, side3)) {
+                    this.side1 = side1;
+                    this.side2 = side2;
+                    this.side3 = side3;
+                } else {
+                    throw new IllegalArgumentException("Invalid triangle sides");
+                }
+            }
+
+            @Override
+            public double getArea() {
+                double s = getPerimeter() / 2.0;
+                return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+            }
+
+            @Override
+            public int getPerimeter() {
+                return side1 + side2 + side3;
+            }
+
+            @Override
+            public boolean isEquilateral() {
+                return side1 == side2 && side2 == side3;
+            }
+
+            @Override
+            public boolean isIsosceles() {
+                return side1 == side2 || side1 == side3 || side2 == side3;
+            }
+
+            @Override
+            public boolean isScalene() {
+                return side1 != side2 && side1 != side3 && side2 != side3;
+            }
+
+            @Override
+            public boolean sidesFormTriangle(int side1, int side2, int side3) {
+                return side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1;
+            }
+
+            @Override
+            public boolean pointsFormTriangle(int x1, int y1, int x2, int y2, int x3, int y3) {
+                return true;
+            }
+
+            @Override
+            public String triangleType(int side1, int side2, int side3) {
+                if (isEquilateral()) {
+                    return "Equilateral";
+                } else if (isIsosceles()) {
+                    return "Isosceles";
+                } else {
+                    return "Scalene";
+                }
+            }
+
+            @Override
+            public int squaredHypotenuse(int side1, int side2) {
+                return side1 * side1 + side2 * side2;
+            }
+        };
     }
 
     @Test
