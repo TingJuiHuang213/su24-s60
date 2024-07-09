c1f6d6b50239cd002d44357c44c6791d56d5fa71
your name <rrr870213@berkeley.edu>
2024-07-09 13:53:41 -0700

    Finished Lab 12

--- lab12/src/RedBlackTree.java
+++ lab12/src/RedBlackTree.java
@@ -10,16 +10,13 @@ public class RedBlackTree<T extends Comparable<T>> {
         RBTreeNode<T> left;
         RBTreeNode<T> right;
 
-        /** Creates a RBTreeNode with item ITEM and color depending on ISBLACK
-           value. */
+        /** Creates a RBTreeNode with item ITEM and color depending on ISBLACK value. */
         RBTreeNode(boolean isBlack, T item) {
             this(isBlack, item, null, null);
         }
 
-        /** Creates a RBTreeNode with item ITEM, color depending on ISBLACK
-           value, left child LEFT, and right child RIGHT. */
-        RBTreeNode(boolean isBlack, T item, RBTreeNode<T> left,
-                   RBTreeNode<T> right) {
+        /** Creates a RBTreeNode with item ITEM, color depending on ISBLACK value, left child LEFT, and right child RIGHT. */
+        RBTreeNode(boolean isBlack, T item, RBTreeNode<T> left, RBTreeNode<T> right) {
             this.isBlack = isBlack;
             this.item = item;
             this.left = left;
@@ -38,40 +35,61 @@ public class RedBlackTree<T extends Comparable<T>> {
         root = buildRedBlackTree(ttTreeRoot);
     }
 
-    /** Builds a RedBlackTree that has isometry with given 2-3 tree rooted at
-       given node R, and returns the root node. */
+    /** Builds a RedBlackTree that has isometry with given 2-3 tree rooted at given node R, and returns the root node. */
     RBTreeNode<T> buildRedBlackTree(Node<T> r) {
         if (r == null) {
             return null;
         }
 
         if (r.getItemCount() == 1) {
-            // TODO: Replace with code to create a 2-node equivalent
-            return null;
+            // 處理2-node的情況
+            T item = r.getItemAt(0);
+            RBTreeNode<T> newNode = new RBTreeNode<>(true, item);
+            newNode.left = buildRedBlackTree(r.getChildAt(0));
+            newNode.right = buildRedBlackTree(r.getChildAt(1));
+            return newNode;
         } else {
-            // TODO: Replace with code to create a 3-node equivalent
-            return null;
+            // 處理3-node的情況
+            T leftItem = r.getItemAt(0);
+            T rightItem = r.getItemAt(1);
+            RBTreeNode<T> leftNode = new RBTreeNode<>(false, leftItem);
+            RBTreeNode<T> parentNode = new RBTreeNode<>(true, rightItem, leftNode, null);
+            leftNode.left = buildRedBlackTree(r.getChildAt(0));
+            leftNode.right = buildRedBlackTree(r.getChildAt(1));
+            parentNode.right = buildRedBlackTree(r.getChildAt(2));
+            return parentNode;
         }
     }
 
-    /** Flips the color of NODE and its children. Assume that NODE has both left
-       and right children. */
+    /** Flips the color of NODE and its children. Assume that NODE has both left and right children. */
     void flipColors(RBTreeNode<T> node) {
-        // TODO: YOUR CODE HERE
+        node.isBlack = !node.isBlack;
+        if (node.left != null) {
+            node.left.isBlack = !node.left.isBlack;
+        }
+        if (node.right != null) {
+            node.right.isBlack = !node.right.isBlack;
+        }
     }
 
-    /** Rotates the given node NODE to the right. Returns the new root node of
-       this subtree. */
+    /** Rotates the given node NODE to the right. Returns the new root node of this subtree. */
     RBTreeNode<T> rotateRight(RBTreeNode<T> node) {
-        // TODO: YOUR CODE HERE
-        return null;
+        RBTreeNode<T> x = node.left;
+        node.left = x.right;
+        x.right = node;
+        x.isBlack = node.isBlack;
+        node.isBlack = false;
+        return x;
     }
 
-    /** Rotates the given node NODE to the left. Returns the new root node of
-       this subtree. */
+    /** Rotates the given node NODE to the left. Returns the new root node of this subtree. */
     RBTreeNode<T> rotateLeft(RBTreeNode<T> node) {
-        // TODO: YOUR CODE HERE
-        return null;
+        RBTreeNode<T> x = node.right;
+        node.right = x.left;
+        x.left = node;
+        x.isBlack = node.isBlack;
+        node.isBlack = false;
+        return x;
     }
 
     public void insert(T item) {
@@ -79,35 +97,35 @@ public class RedBlackTree<T extends Comparable<T>> {
         root.isBlack = true;
     }
 
-    /** Inserts the given node into this Red Black Tree*/
+    /** Inserts the given node into this Red Black Tree */
     private RBTreeNode<T> insert(RBTreeNode<T> node, T item) {
-        // Insert (return) new red leaf node.
         if (node == null) {
             return new RBTreeNode<>(false, item);
         }
 
-        // Handle normal binary search tree insertion.
         int comp = item.compareTo(node.item);
-        if (comp == 0) {
-            return node; // do nothing.
-        } else if (comp < 0) {
+        if (comp < 0) {
             node.left = insert(node.left, item);
-        } else {
+        } else if (comp > 0) {
             node.right = insert(node.right, item);
+        } else {
+            return node;
         }
 
-        // handle "middle of three" and "right-leaning red" structures
+        if (isRed(node.right) && !isRed(node.left)) {
+            node = rotateLeft(node);
+        }
+        if (isRed(node.left) && isRed(node.left.left)) {
+            node = rotateRight(node);
+        }
+        if (isRed(node.left) && isRed(node.right)) {
+            flipColors(node);
+        }
 
-        // handle "smallest of three" structure
-
-        // handle "largest of three" structure
-
-        // TODO: YOUR CODE HERE
-        return null; //fix this return statement
+        return node;
     }
 
-    /** Returns whether the given node NODE is red. Null nodes (children of leaf
-       nodes are automatically considered black. */
+    /** Returns whether the given node NODE is red. Null nodes (children of leaf nodes are automatically considered black. */
     private boolean isRed(RBTreeNode<T> node) {
         return node != null && !node.isBlack;
     }
