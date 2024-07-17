6fa896b0f17ac715951e388f9a5fea75c348d2b2
your name <rrr870213@berkeley.edu>
2024-07-17 15:43:46 -0700

    Finished Lab 15

--- lab15/src/UnionFind.java
+++ lab15/src/UnionFind.java
@@ -2,6 +2,8 @@ public class UnionFind {
     private int[] parent;
     private int[] size;
 
+    /* Creates a UnionFind data structure holding N items. Initially, all
+       items are in disjoint sets. */
     public UnionFind(int N) {
         parent = new int[N];
         size = new int[N];
@@ -11,29 +13,45 @@ public class UnionFind {
         }
     }
 
+    /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
         int root = find(v);
         return size[root];
     }
 
+    /* Returns the parent of V. If V is the root of a tree, returns the
+       negative size of the tree for which V is the root. */
     public int parent(int v) {
-        return parent[v] == v ? -size[v] : parent[v];
+        if (v == parent[v]) {
+            return -size[v];
+        } else {
+            return parent[v];
+        }
     }
 
+    /* Returns true if nodes V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
         return find(v1) == find(v2);
     }
 
+    /* Returns the root of the set V belongs to. Path-compression is employed
+       allowing for fast search-time. If invalid items are passed into this
+       function, throw an IllegalArgumentException. */
     public int find(int v) {
         if (v < 0 || v >= parent.length) {
             throw new IllegalArgumentException("Invalid index");
         }
         if (parent[v] != v) {
-            parent[v] = find(parent[v]);
+            parent[v] = find(parent[v]); // Path compression
         }
         return parent[v];
     }
 
+    /* Connects two items V1 and V2 together by connecting their respective
+       sets. V1 and V2 can be any element, and a union-by-size heuristic is
+       used. If the sizes of the sets are equal, tie break by connecting V1's
+       root to V2's root. Union-ing a item with itself or items that are
+       already connected should not change the structure. */
     public void union(int v1, int v2) {
         int root1 = find(v1);
         int root2 = find(v2);
